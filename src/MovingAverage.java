02e7e21f4ca7553f40f1eae4848cdd03dc9b3dd3
zongjunhao <1016767058@qq.com>
2022-07-17 12:41:49 +0800

    Moving Average

---
+++ src/MovingAverage.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class MovingAverage {
+    double[] doubleArray;
+    int filledPosNum = 0;
+    int pos = 0;
+    int size;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MovingAverage(int size) {
+        this.size = size;
+        this.doubleArray = new double[size];
+        Arrays.fill(doubleArray, 0);
+    }
+
+    public double next(int val) {
+        if (filledPosNum < size) {
+            filledPosNum++;
+        }
+        doubleArray[pos] = val;
+        pos = (pos + 1) % size;
+        double sum = 0;
+        for (int i = 0; i < filledPosNum; i++) {
+            sum += doubleArray[i];
+        }
+        return sum / filledPosNum;
+    }
+
+    public static void main(String[] args) {
+        int size = 3;
+        MovingAverage movingAverage = new MovingAverage(size);
+        System.out.println(movingAverage.next(1));  // 返回 1.0 = 1 / 1
+        System.out.println(movingAverage.next(10)); // 返回 5.5 = (1 + 10) / 2
+        System.out.println(movingAverage.next(3)); // 返回 4.66667 = (1 + 10 + 3) / 3
+        System.out.println(movingAverage.next(5)); // 返回 6.0 = (10 + 3 + 5) / 3
+
+    }
+}
